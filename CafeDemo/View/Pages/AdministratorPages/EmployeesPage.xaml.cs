f3ed02b706cf24cf6788cd3fec6a1a01eb7701aa
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-02-28 21:44:32 +0300

    Add filter & search

--- CafeDemo/View/Pages/AdministratorPages/EmployeesPage.xaml.cs
+++ CafeDemo/View/Pages/AdministratorPages/EmployeesPage.xaml.cs
@@ -23,12 +23,23 @@ namespace CafeDemo.View.Pages.AdministratorPages
     /// </summary>
     public partial class EmployeesPage : Page
     {
+        List<Employee> employees = App.context.Employee.ToList();
+        List<Role> roles = App.context.Role.ToList();
         public EmployeesPage()
         {
             InitializeComponent();
 
-            EmployeesLv.ItemsSource = App.context.Employee.ToList();
+            EmployeesLv.ItemsSource = employees;
             EmployeesLv.SelectedIndex = 0;
+
+            // Программно добавляем пункт для выбора пользователей всех должностей.
+            roles.Insert(0, new Role { Name = "Все должности" });
+
+            // Заполнение выпадающего списка для поиска.
+            FilterByRoleCmb.ItemsSource = roles;
+            FilterByRoleCmb.DisplayMemberPath = "Name";
+            FilterByRoleCmb.SelectedValuePath = "Id";
+            FilterByRoleCmb.SelectedIndex = 0;
         }
 
         private void AddNewEmployeeBtn_Click(object sender, RoutedEventArgs e)
@@ -39,12 +50,12 @@ namespace CafeDemo.View.Pages.AdministratorPages
 
         private void SaveEmployeeBtn_Click(object sender, RoutedEventArgs e)
         {
-            // Подхватываем изменения в роль из комбобокса
+            // Подхватываем изменения в роль из выпадающего списка.
             (EmployeesLv.SelectedItem as Employee).RoleId = Convert.ToInt32(RoleCmb.SelectedValue);
 
             App.context.SaveChanges();
 
-            EmployeesLv.ItemsSource = App.context.Employee.ToList();
+            EmployeesLv.ItemsSource = employees = App.context.Employee.ToList();
 
             MessageBoxHelper.Information("Данные пользователя изменены!");
         }
@@ -57,7 +68,7 @@ namespace CafeDemo.View.Pages.AdministratorPages
             RoleCmb.DisplayMemberPath = "Name";
             RoleCmb.SelectedValuePath = "Id";
             // Влияет на поиск. Выдаёт исключение типа NullReferenceException.
-            // Если при поиске выбранный пользователь пропадает из поиска, то нужно убрать индекс выбранного элемента из комбобокса.
+            // Если при поиске выбранный пользователь пропадает из списка, то нужно установить индекс на -1 (убрать выбор) из выпадающего списка.
             RoleCmb.SelectedIndex = EmployeesLv.SelectedItem as Employee != null ? (EmployeesLv.SelectedItem as Employee).RoleId - 1 : -1;
         }
 
@@ -82,11 +93,25 @@ namespace CafeDemo.View.Pages.AdministratorPages
         {
             if (SearchTb.Text != string.Empty)
             {
-                EmployeesLv.ItemsSource = App.context.Employee.Where(employee => employee.Name.ToLower().Contains(SearchTb.Text.ToLower())).ToList();
+                EmployeesLv.ItemsSource = employees = employees.Where(employee => employee.Name.ToLower().Contains(SearchTb.Text.ToLower())).ToList();
             }
             else
             {
-                EmployeesLv.ItemsSource = App.context.Employee.ToList();
+                EmployeesLv.ItemsSource = employees = App.context.Employee.ToList();
+            }
+        }
+        #endregion
+
+        #region Фильтрация
+        private void FilterByRoleCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FilterByRoleCmb.SelectedIndex != 0)
+            {
+                EmployeesLv.ItemsSource = employees.Where(employee => employee.RoleId == FilterByRoleCmb.SelectedIndex).ToList();
+            }
+            else
+            {
+                EmployeesLv.ItemsSource = employees;
             }
         }
         #endregion
